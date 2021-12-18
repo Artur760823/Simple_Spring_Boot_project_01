85889d7ec3101458cda006ac7eb64e4fecfde7c6
Artur <artur.zajaczkowski76@gmail.com>
2021-12-18 17:07:52 +0100

    First output on localhost:8080

--- src/main/java/com/codeRabbit/demo/SimpleSpringBootProject01Application.java
+++ src/main/java/com/codeRabbit/demo/SimpleSpringBootProject01Application.java
@@ -2,12 +2,23 @@ package com.codeRabbit.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SimpleSpringBootProject01Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SimpleSpringBootProject01Application.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello",  "Wolrd");
 	}
 
 }
