88e2b8dae0324c3d2313350e4dbd544af431a723
Artur <artur.zajaczkowski76@gmail.com>
2021-12-18 16:57:09 +0100

    Initial commit

---
+++ src/main/java/com/codeRabbit/demo/SimpleSpringBootProject01Application.java
@@ -0,0 +1,13 @@
+package com.codeRabbit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeRabbit/demo/SimpleSpringBootProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeRabbit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
